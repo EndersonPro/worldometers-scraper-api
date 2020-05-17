9490d29670f93f5553098d4e6f9daf975bcdb39e
EndersonPro <enjaviga@gmail.com>
2020-05-16 21:48:03 -0500

    Removiendo rutas apis para manejarlas por separado, obteniendo variables de entorno

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType
 from ariadne.constants import PLAYGROUND_HTML
 from flask import Flask, jsonify, request
@@ -7,15 +8,24 @@ from redis import Redis
 import rx
 from rx import operators as ops
 import json
+from flask_restful import Resource, Api
+from resources import Worldometers
 
 app = Flask(__name__)
+api = Api(app)
+
+# API RESOURCES CONFIG
+api.add_resource(Worldometers, '/api')
+
 
 # Creating a subscriber to get the information every 5 minutes
 o = rx.interval(300)
 sub = o.subscribe(lambda _: update_data_redis())
 
 # Redis instance
-r = Redis(host='localhost', port='6379')
+REDIS_HOST = os.getenv('REDIS_HOST') if not os.getenv('REDIS_HOST') == None else 'localhost'
+REDIS_PORT = os.getenv('REDIS_PORT') if not os.getenv('REDIS_PORT') == None else '6379'
+r = Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 @app.route('/graphql', methods=['GET'])
 def playground():
@@ -35,7 +45,6 @@ def graphql_server():
     query.set_field('continent_by_name_resolver', resolver.continent_by_name_resolver)
 
     schema = make_executable_schema(type_defs, [continent, country, query])
-
     data = request.get_json()
     success, result = graphql_sync(
         schema,
@@ -45,16 +54,6 @@ def graphql_server():
     status_code = 200 if success else 400
     return jsonify(result), status_code
 
-@app.route('/api')
-def index():
-    try:
-        co = json.loads(r.get('countries'))
-        cs = json.loads(r.get('continents'))
-        totalr = json.loads(r.get('total'))
-        return jsonify({ 'data': { 'countries': co, 'continents': cs, 'total': totalr }})
-    except:
-        sub.unsubscribe()
-        return jsonify({ 'data': '', 'message': 'An error ocurred.' })
 
 def update_data_redis():
     s = Scraper()
@@ -65,5 +64,8 @@ def update_data_redis():
 
 if __name__ == '__main__':
     update_data_redis()
-    # app.run(host='0.0.0.0', port=80)
-    app.run()
+    isProduction = os.getenv('PRODUCTION') if not os.getenv('PRODUCTION') == None else False
+    if isProduction:
+        app.run(host='0.0.0.0', port=80)
+    else:
+        app.run()
